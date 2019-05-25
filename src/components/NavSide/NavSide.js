5926ccb6b0b25658ad6838ef6baee3a09ed64689
quanghuy1242 <katygaga124@gmail.com>
2019-05-25 17:37:43 +0700

    added: NavSide component

---
+++ src/components/NavSide/NavSide.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Nav, Icon, initializeIcons } from 'office-ui-fabric-react';
+import { getStyle } from './NavSide.style';
+import { Link, withRouter } from 'react-router-dom';
+
+initializeIcons();
+
+class NavSide extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: [
+        {
+          name: 'Home',
+          url: '/',
+          key: "keyHome",
+          icon: 'Home'
+        },
+        {
+          name: 'Categories',
+          url: '/categories',
+          key: 'keyCategories',
+          icon: 'Taskboard'
+        },
+        {
+          name: 'About',
+          url: '/about',
+          key: 'keyAbout',
+          icon: 'Info'
+        }
+      ],
+      selectedKey: null
+    };
+  }
+
+  componentDidMount() {
+    this.props.history.listen((location, action) => {
+      const currentRoute = this.state.links.find(link => link.url === location.pathname);
+      if (currentRoute) {
+        this.setState({
+          selectedKey: currentRoute.key
+        });
+        this.props.isOverlay && !this.props.isCollapsed && this.props.onDismiss(); // đóng navside chỉ khi navside đang mở
+      };
+    })
+  }
+
+  onRenderLink = (props) => {
+    return (
+      <Link className={props.className} style={{color: 'inherit', boxSizing: 'border-box'}} to={props.href}>
+        <span style={{display: 'flex'}}>
+          { props.iconProps && <Icon style={{margin: '0 10px', fontSize: '20px'}} {...props.iconProps} /> }
+          {props.children}
+        </span>
+      </Link>
+    );
+  }
+
+  render() {
+    let selectedKey = this.state.selectedKey ? this.state.selectedKey : null;
+    const classNames = getStyle(this.props);
+    return (
+      <div>
+        <div className={classNames.navWrapper}>
+          <Nav
+            expandedStateText="expanded"
+            collapsedStateText="collapsed"
+            {...selectedKey}
+            onLinkClick={() => {}}
+            styles={{
+              root: {
+                width: this.props.isCollapsed ? 0 : 250,
+                height: '100%',
+                boxSizing: 'border-box',
+                overflowY: 'auto',
+                transition: '0.2s',
+                zIndex: 98
+              }
+            }}
+            groups={[{ links: this.state.links }]}
+            linkAs={this.onRenderLink}
+          />
+        </div>
+        {
+          this.props.isOverlay
+            ? !this.props.isCollapsed
+              ? <div className={classNames.navOverlay} onClick={this.props.onDismiss}></div>
+              : <></>
+            : <></>
+        }
+      </div>
+    );
+  }
+}
+
+export default withRouter(NavSide);
---
+++ src/components/NavSide/NavSide.style.js
@@ -0,0 +1,31 @@
+import { mergeStyleSets } from 'office-ui-fabric-react'
+
+export const getStyle = ({ isCollapsed, isOverlay }) => {
+  const customDepth = 'rgba(0, 0, 0, 0.22) 0px 50px 55px 0px, rgba(0, 0, 0, 0.18) 0px 15px 15px 0px';
+
+  return mergeStyleSets({
+    navWrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: 'calc(100vh - 55px)',
+      transition: '0.2s',
+      backgroundColor: 'white',
+      zIndex: 100,
+      width: isCollapsed ? 0 : 250,
+      position: isOverlay ? 'absolute' : 'initial',
+      boxShadow: isOverlay ? customDepth : 'none',
+      border: isCollapsed ? 'none' : '1px solid #eee',
+    },
+    navOverlay: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: '#eee',
+      opacity: 0.3,
+      zIndex: 97
+    }
+  })
+}
