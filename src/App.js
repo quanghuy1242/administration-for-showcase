b9db25b6811944c707533706a44bbde24d845623
quanghuy1242 <katygaga124@gmail.com>
2019-05-25 14:51:06 +0700

    added: router container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Customizer } from 'office-ui-fabric-react';
+import { FluentCustomizations } from '@uifabric/fluent-theme';
+import RouteContainer from './components/RouteContainer/RouteContainer';
 
 class App extends React.Component {
-  
+  render() {
+    return (
+      <Customizer {...FluentCustomizations}>
+        <RouteContainer />
+      </Customizer>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/RouteContainer/RouteContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default class RouteContainer extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={() => (<div>Home</div>)} />
+        </Switch>
+      </Router>
+    );
+  }
+}
