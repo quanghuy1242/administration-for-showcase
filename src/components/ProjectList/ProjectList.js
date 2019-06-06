34bb14d83dbc71702f28d05c4e08f035f29bd449
quanghuy1242 <katygaga124@gmail.com>
2019-06-06 17:42:02 +0700

    added: filter section to project list, updated with too much items in list...

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import { ProjectItem } from '../ProjectItem/ProjectItem';
 import { getStyle } from './ProjectList.style';
+import { Stack, SearchBox, IconButton } from 'office-ui-fabric-react';
 
 export class ProjectList extends React.Component {
   render() {
     const { projects, selected } = this.props;
     const classNames = getStyle();
     return (
-      <div>
-        {projects.map(project => (
-          <div
-            onClick={() => this.props.onSelectedProjectChanged(project.id)}
-            key={project.id}
-          >
-            <ProjectItem
-              project={project}
-              selected={selected}
-              className={parseInt(selected) === parseInt(project.id) ? classNames.actived : ''}
+      <Stack>
+        <Stack horizontal className={classNames.filterSession}>
+          <Stack.Item grow disableShrink>
+            <SearchBox
+              placeholder="Filter..."
             />
-          </div>
-        ))}
-      </div>
+          </Stack.Item>
+          <Stack.Item>
+            <IconButton
+              iconProps={{ iconName: 'MoreVertical' }}
+            />
+          </Stack.Item>
+        </Stack>
+        <div className={classNames.projectsListWrapper}>
+          {projects.map(project => (
+            <div
+              onClick={() => this.props.onSelectedProjectChanged(project.id)}
+              key={project.id}
+            >
+              <ProjectItem
+                project={project}
+                selected={selected}
+                className={parseInt(selected) === parseInt(project.id) ? classNames.actived : ''}
+              />
+            </div>
+          ))}
+        </div>
+      </Stack>
     );
   }
 }
--- src/components/ProjectList/ProjectList.style.js
+++ src/components/ProjectList/ProjectList.style.js
@@ -2,8 +2,20 @@ import { mergeStyleSets } from 'office-ui-fabric-react';
 
 export const getStyle = () => {
   return mergeStyleSets({
+    projectsListWrapper: {
+      overflowY: 'auto',
+      height: 'calc(100vh - 55px - 48px)'
+    },
     actived: {
-      backgroundColor: 'yellow'
+      backgroundColor: 'rgba(89, 144, 180, 0.377) !important',
+      selectors: {
+        "& span:nth-child(1)": {
+          fontWeight: '800'
+        }
+      }
+    },
+    filterSession: {
+      margin: '0.5rem 0 0.5rem 0.5rem'
     }
   })
 }
