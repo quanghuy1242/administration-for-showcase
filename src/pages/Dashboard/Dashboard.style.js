ac0cbf81364c0b83a29720b0fa82317008d1f03a
quanghuy1242 <katygaga124@gmail.com>
2019-06-07 14:35:58 +0700

    added: box-shadow to right panel

--- src/pages/Dashboard/Dashboard.style.js
+++ src/pages/Dashboard/Dashboard.style.js
@@ -1,4 +1,5 @@
 import { mergeStyleSets } from 'office-ui-fabric-react';
+import { Depths } from '@uifabric/fluent-theme';
 
 export const getStyle = () => {
   return mergeStyleSets({
@@ -7,9 +8,10 @@ export const getStyle = () => {
     },
     leftPanel: {
       width: 300,
-      backgroundColor: 'rgba(139, 159, 172, 0.082)',
+      backgroundColor: 'rgba(165, 181, 192, 0.082)'
     },
     rightPanel: {
+      boxShadow: Depths.depth8,
     }
   })
 }
