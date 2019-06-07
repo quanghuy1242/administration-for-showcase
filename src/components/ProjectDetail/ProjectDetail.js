ae8cfce25ce2e6b18b67407d40725160ce513652
quanghuy1242 <katygaga124@gmail.com>
2019-06-07 14:22:31 +0700

    added: template action header to project detail

--- src/components/ProjectDetail/ProjectDetail.js
+++ src/components/ProjectDetail/ProjectDetail.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { Stack, TextField, Pivot, PivotItem } from 'office-ui-fabric-react';
+import { Stack, TextField, Pivot, PivotItem, IconButton } from 'office-ui-fabric-react';
 import { getStyle } from './ProjectDetail.style';
 
 export class ProjectDetail extends React.Component {
   render() {
     const classNames = getStyle();
     return (
-      <Stack className={classNames.projectDetailWrapper} tokens={{ childrenGap: 10 }}>
-        <Stack className={classNames.header} tokens={{ childrenGap: 5 }}>
-          <TextField
-            label="Name of project"
-            value="Unknown Title"
-          />
-          <Pivot>
-            <PivotItem headerText="Description"></PivotItem>
-            <PivotItem headerText="Description"></PivotItem>
-            <PivotItem headerText="Description"></PivotItem>
-            <PivotItem headerText="Description"></PivotItem>
-          </Pivot>
+      <Stack className={classNames.projectDetailWrapper} tokens={{ childrenGap: 5 }}>
+        <Stack horizontal className={classNames.header} tokens={{ childrenGap: 5 }}>
+          <Stack.Item grow disableShrink>
+            <TextField
+              value="Unknown Title"
+            />
+          </Stack.Item>
+          <Stack.Item className={classNames.headerAction}>
+            <IconButton iconProps={{ iconName: 'Edit' }} />
+            <IconButton iconProps={{ iconName: 'Delete' }} />
+            <IconButton iconProps={{ iconName: 'MoreVertical' }} />
+          </Stack.Item>
         </Stack>
+        <Pivot>
+          <PivotItem headerText="Description"></PivotItem>
+          <PivotItem headerText="Description"></PivotItem>
+          <PivotItem headerText="Description"></PivotItem>
+          <PivotItem headerText="Description"></PivotItem>
+        </Pivot>
       </Stack>
     );
   }
--- src/components/ProjectDetail/ProjectDetail.style.js
+++ src/components/ProjectDetail/ProjectDetail.style.js
@@ -7,6 +7,9 @@ export const getStyle = () => {
     },
     header: {
 
+    },
+    headerAction: {
+      // marginTop: 29
     }
   })
 }
