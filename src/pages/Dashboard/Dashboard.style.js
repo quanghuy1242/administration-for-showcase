c49f01ecfa7a2d6e1afd5790f7ace3628ed0164f
quanghuy1242 <katygaga124@gmail.com>
2019-06-11 18:48:37 +0700

    updated: panel box shadow

--- src/pages/Dashboard/Dashboard.style.js
+++ src/pages/Dashboard/Dashboard.style.js
@@ -11,10 +11,10 @@ export const getStyle = () => {
     projectsPanel: {
       width: 270,
       backgroundColor: 'rgba(165, 181, 192, 0.082)',
-      borderLeft: '1px solid rgba(0, 0, 0, 0.082)'
+      boxShadow: 'rgba(0, 0, 0, 0.133) -2.5px 0px 10px 0.5px, rgba(0, 0, 0, 0.11) 0px 0.6px 1.8px 0px'
     },
     projectDetailPanel: {
-      borderLeft: '1px solid rgba(0, 0, 0, 0.082)'
+      boxShadow: 'rgba(0, 0, 0, 0.133) -2.5px 0px 10px 0.5px, rgba(0, 0, 0, 0.11) 0px 0.6px 1.8px 0px'
     }
   })
 }
