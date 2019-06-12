a35c240be747a805de0ac9e3ef35db565ea5bd15
quanghuy1242 <katygaga124@gmail.com>
2019-06-12 15:41:19 +0700

    updated: layout style

--- src/pages/Dashboard/Dashboard.style.js
+++ src/pages/Dashboard/Dashboard.style.js
@@ -6,15 +6,19 @@ export const getStyle = () => {
       width: '100%'
     },
     technologiesPanel: {
-      width: 270
+      width: 250,
+      backgroundColor: 'rgba(165, 181, 192, 0.082)',
+      boxShadow: 'rgba(0, 0, 0, 0.133) -5px 0px 5px -5px',
+      borderTop: '1px solid rgba(0, 0, 0, 0.12)'
     },
     projectsPanel: {
       width: 270,
+      borderTop: '1px solid rgba(0, 0, 0, 0.12)',
       backgroundColor: 'rgba(165, 181, 192, 0.082)',
-      boxShadow: 'rgba(0, 0, 0, 0.133) -2.5px 0px 10px 0.5px, rgba(0, 0, 0, 0.11) 0px 0.6px 1.8px 0px'
+      borderLeft: '1px solid rgba(0, 0, 0, 0.12)'
     },
     projectDetailPanel: {
-      boxShadow: 'rgba(0, 0, 0, 0.133) -2.5px 0px 10px 0.5px, rgba(0, 0, 0, 0.11) 0px 0.6px 1.8px 0px'
+      borderLeft: '1px solid rgba(0, 0, 0, 0.12)'
     }
   })
 }
