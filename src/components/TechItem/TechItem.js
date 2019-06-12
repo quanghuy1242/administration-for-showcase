ca88887b8714f41879adcd569019f6132454cd82
quanghuy1242 <katygaga124@gmail.com>
2019-06-12 18:47:45 +0700

    updated: more button only show if it is actived

--- src/components/TechItem/TechItem.js
+++ src/components/TechItem/TechItem.js
@@ -4,15 +4,19 @@ import { getStyle } from './TechItem.style';
 
 export class TechItem extends React.Component {
   render() {
-    const { tech, className } = this.props;
+    const { tech, className, selected } = this.props;
     const classNames = getStyle();
     return (
       <Stack className={css(className, classNames.projectItem)} verticalAlign="center">
         <Text variant="medium" className={classNames.projectItem_Header} nowrap>{tech.name}</Text>
-        <IconButton
-          iconProps={{ iconName: 'MoreVertical' }}
-          className={classNames.moreButton}
-        />
+        {parseInt(selected) === parseInt(tech.id)
+          ? (
+            <IconButton
+              iconProps={{ iconName: 'MoreVertical' }}
+              className={classNames.moreButton}
+            />
+          )
+          : <></>}
       </Stack>
     );
   }
