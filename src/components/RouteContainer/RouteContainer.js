9aaf271b2fd93b1b4a70aab7797e4e8d39950201
quanghuy1242 <katygaga124@gmail.com>
2019-06-13 15:22:22 +0700

    updated: add login page

--- src/components/RouteContainer/RouteContainer.js
+++ src/components/RouteContainer/RouteContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Dashboard } from '../../pages/Dashboard/Dashboard';
+import { Login } from '../../pages/Login/Login';
 
 export class RouteContainer extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Dashboard} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     );
   }
