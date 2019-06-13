e4b3e7197c12b63b1d6a33b9d128de6206ea64da
quanghuy1242 <katygaga124@gmail.com>
2019-06-13 18:47:16 +0700

    updated: add / route

--- src/components/RouteContainer/RouteContainer.js
+++ src/components/RouteContainer/RouteContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Dashboard } from '../../pages/Dashboard/Dashboard';
 import { Login } from '../../pages/Login/Login';
+import { LandscapeHome } from '../../pages/LandscapeHome/LandscapeHome';
 
 export class RouteContainer extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={LandscapeHome} />
+        <Route exact path="/projects" component={Dashboard} />
         <Route exact path="/login" component={Login} />
       </Switch>
     );
