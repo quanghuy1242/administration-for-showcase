baf9dd5750ecc7fe0c03ddf9f3e5e97acf825b49
quanghuy1242 <katygaga124@gmail.com>
2019-06-14 23:03:33 +0700

    updated: project list with loading placeholder

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { ProjectItem } from '../ProjectItem/ProjectItem';
 import { getStyle } from './ProjectList.style';
 import { Stack } from 'office-ui-fabric-react';
+import { ShimmerLoading } from '../ShimmerLoading/ShimmerLoading';
 
 export class ProjectList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wrapperHeight: undefined,
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ wrapperHeight: this.wrapper.clientHeight });
+  }
+
   render() {
     const { projects, selected } = this.props;
     const classNames = getStyle();
     return (
       <Stack>
-        <div className={classNames.projectsListWrapper}>
+        {this.state.wrapperHeight && this.state.isLoading
+          ? <ShimmerLoading length={Math.floor(this.state.wrapperHeight / 58)} />
+          : <></>}
+        <div className={classNames.projectsListWrapper} ref={ (wrapper) => this.wrapper = wrapper }>
           {projects.map(project => (
             <div
               onClick={() => this.props.onSelectedProjectChanged(project.id)}
