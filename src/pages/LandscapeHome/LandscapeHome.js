cd9e4a7fb6a2e6162f5ff1f58d59aa0a87cce724
quanghuy1242 <katygaga124@gmail.com>
2019-06-14 11:09:02 +0700

    updated: homepage

--- src/pages/LandscapeHome/LandscapeHome.js
+++ src/pages/LandscapeHome/LandscapeHome.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Stack } from 'office-ui-fabric-react';
+import { Stack, Text, PrimaryButton } from 'office-ui-fabric-react';
 import { getStyle } from './LandscapeHome.style';
+import logo from './image.svg';
+import { Link } from 'react-router-dom';
 
 export class LandscapeHome extends React.Component {
   render() {
     const classNames = getStyle();
     return (
-      <Stack>
-        Home
+      <Stack horizontal className={classNames.homeWrapper}>
+        <Stack.Item className={classNames.leftPanel}>
+          <Stack horizontalAlign="center" verticalAlign='center' className={classNames.leftPanelInner}>
+            <Text variant="superLarge">Administration</Text>
+            <Text variant="superLarge">for Project Showcase</Text>
+            <Link className={classNames.button} to='/projects'>
+              <PrimaryButton>Get into your work</PrimaryButton>
+            </Link>
+          </Stack>
+        </Stack.Item>
+        <Stack.Item grow disableShrink className={classNames.rightPanel}>
+          <Stack verticalAlign="center" horizontalAlign="start" className={classNames.rightPanelInner} >
+            <img src={logo} alt="hmm" style={{ width: 550 }} />
+          </Stack>
+        </Stack.Item>
       </Stack>
     );
   }
--- src/pages/LandscapeHome/LandscapeHome.style.js
+++ src/pages/LandscapeHome/LandscapeHome.style.js
@@ -2,6 +2,23 @@ import { mergeStyleSets } from 'office-ui-fabric-react';
 
 export const getStyle = () => {
   return mergeStyleSets({
-    
+    homeWrapper: {
+      width: '100%',
+      height: 'calc(100vh - 55px)',
+      padding: '2rem'
+    },
+    leftPanel: {
+      width: 'calc(100vw - 800px)'
+    },
+    leftPanelInner: {
+      height: '100%'
+    },
+    rightPanelInner: {
+      height: '100%',
+      margin: '1rem'
+    },
+    button: {
+      marginTop: '1.5rem !important'
+    }
   })
 }
