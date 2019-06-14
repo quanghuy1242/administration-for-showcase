d179520383addac48afd1c64c56b4fa85cc7930a
quanghuy1242 <katygaga124@gmail.com>
2019-06-14 19:11:51 +0700

    added: props classname

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { style } from './NavLink.style';
 import { Link } from 'react-router-dom';
+import { css } from '@uifabric/utilities';
 
 export class NavLink extends Component {
   render() {
     return (
-      <Link to={this.props.href} className={style.navLink}>
+      <Link to={this.props.href} className={css(style.navLink, this.props.className)}>
         {this.props.children}
       </Link>
     );
