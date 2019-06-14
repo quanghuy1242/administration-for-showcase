394219b20fafb7ad52a9b63d852517becadc43e7
quanghuy1242 <katygaga124@gmail.com>
2019-06-14 23:03:06 +0700

    added: Shimmer loading

---
+++ src/components/ShimmerLoading/ShimmerLoading.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Stack, Shimmer } from 'office-ui-fabric-react';
+
+export class ShimmerLoading extends React.Component {
+  render() {
+    return (
+      <Stack tokens={{ childrenGap: 20 }} style={{ margin: '0.5rem' }}>
+        {[...Array(this.props.length)].map((v, i) => (
+          <div key={i}>
+            <Stack tokens={{ childrenGap: 10 }}>
+              <Shimmer />
+              <Shimmer width="75%" />
+            </Stack>
+          </div>
+        ))}
+      </Stack>
+    );
+  }
+}
