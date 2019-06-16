e035ebc31b09cdb21af02587b43b4a9c94fd2ed5
quanghuy1242 <katygaga124@gmail.com>
2019-06-16 13:52:28 +0700

    updated: add content for other tab

--- src/components/ProjectDetail/ProjectDetail.js
+++ src/components/ProjectDetail/ProjectDetail.js
@@ -3,8 +3,16 @@ import { Stack, Pivot, PivotItem, PivotLinkFormat } from 'office-ui-fabric-react
 import { getStyle } from './ProjectDetail.style';
 import { ProjectBasicInfo } from '../ProjectBasicInfo/ProjectBasicInfo';
 import { ProjectScreenshots } from '../ProjectScreenshots/ProjectScreenshots';
+import { MonacoEditor } from '../MonacoEditor/MonacoEditor';
 
 export class ProjectDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Ví dụ"
+    };
+  };
+
   render() {
     const classNames = getStyle();
     return (
@@ -15,13 +23,19 @@ export class ProjectDetail extends React.Component {
             <ProjectScreenshots />
           </PivotItem>
           <PivotItem headerText="Description">
-
+            <Stack>
+              <MonacoEditor value={this.state.value} />
+            </Stack>
           </PivotItem>
           <PivotItem headerText="Features">
-
+            <Stack>
+              <MonacoEditor value={this.state.value} />
+            </Stack>
           </PivotItem>
           <PivotItem headerText="Changelog">
-
+            <Stack>
+              <MonacoEditor value={this.state.value} />
+            </Stack>
           </PivotItem>
         </Pivot>
       </Stack>
