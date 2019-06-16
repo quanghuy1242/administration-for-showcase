32441f0efcae41efb4d765740a55a857ee445aed
quanghuy1242 <katygaga124@gmail.com>
2019-06-16 16:09:44 +0700

    updated: larger margin

--- src/components/ShimmerLoading/ShimmerLoading.js
+++ src/components/ShimmerLoading/ShimmerLoading.js
@@ -4,7 +4,7 @@ import { Stack, Shimmer } from 'office-ui-fabric-react';
 export class ShimmerLoading extends React.Component {
   render() {
     return (
-      <Stack tokens={{ childrenGap: 20 }} style={{ margin: '0.5rem' }}>
+      <Stack tokens={{ childrenGap: 20 }} style={{ margin: '1rem' }}>
         {[...Array(this.props.length)].map((v, i) => (
           <div key={i}>
             <Stack tokens={{ childrenGap: 10 }}>
