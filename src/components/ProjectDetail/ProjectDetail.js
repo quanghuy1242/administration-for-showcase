f71b1a79a8651c05e6be6a8d3954991e9e451930
quanghuy1242 <katygaga124@gmail.com>
2019-06-17 13:09:35 +0700

    updated: add project description comp

--- src/components/ProjectDetail/ProjectDetail.js
+++ src/components/ProjectDetail/ProjectDetail.js
@@ -3,9 +3,9 @@ import { Stack, Pivot, PivotItem, PivotLinkFormat } from 'office-ui-fabric-react
 import { getStyle } from './ProjectDetail.style';
 import { ProjectBasicInfo } from '../ProjectBasicInfo/ProjectBasicInfo';
 import { ProjectScreenshots } from '../ProjectScreenshots/ProjectScreenshots';
-import { MonacoEditor } from '../MonacoEditor/MonacoEditor';
 import { AppContext } from '../../context/AppContext';
 import { ProjectInfomation } from '../ProjectInfomation/ProjectInfomation';
+import { ProjectDescription } from '../ProjectDescription/ProjectDescription';
 
 export class ProjectDetail extends React.Component {
   render() {
@@ -22,17 +22,17 @@ export class ProjectDetail extends React.Component {
           </PivotItem>
           <PivotItem headerText="Description">
             <Stack>
-              <MonacoEditor value={this.context.selectedProjectDetail.description} />
+              <ProjectDescription />
             </Stack>
           </PivotItem>
           <PivotItem headerText="Features">
             <Stack>
-              <MonacoEditor value={this.context.selectedProjectDetail.description} />
+              <ProjectDescription />
             </Stack>
           </PivotItem>
           <PivotItem headerText="Changelog">
             <Stack>
-              <MonacoEditor value={this.context.selectedProjectDetail.description} />
+              <ProjectDescription />
             </Stack>
           </PivotItem>
         </Pivot>
