539b30e2f80c8a52fd3bb90f7ae21e51ea4cdd07
quanghuy1242 <katygaga124@gmail.com>
2019-06-21 20:03:10 +0700

    updated: toolbar template

--- src/components/MonacoEditor/MonacoEditor.js
+++ src/components/MonacoEditor/MonacoEditor.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Stack, IconButton } from 'office-ui-fabric-react';
+import { Stack, IconButton, TooltipHost, Text } from 'office-ui-fabric-react';
 import { getStyle } from './MonacoEditor.style';
+import { CommunicationColors } from '@uifabric/fluent-theme';
 
 export class MonacoEditor extends React.Component {
   static defaultProps = {
@@ -158,7 +159,8 @@ export class MonacoEditor extends React.Component {
 
   action = {
     bold: () => this.actionWrapper(/^\*\*(.*)\*\*$/, '**'),
-    italic: () => this.actionWrapper(/^_(.*)_$/, '_')
+    italic: () => this.actionWrapper(/^_(.*)_$/, '_'),
+    strikethrough: () => this.actionWrapper(/^~~(.*)~~$/, '~~'),
   }
 
   render() {
@@ -167,17 +169,174 @@ export class MonacoEditor extends React.Component {
     return (
       <Stack>
         <Stack horizontal className={classNames.toolbar}>
-          <IconButton
-            iconProps={{ iconName: "Save" }}
-          />
-          <IconButton
-            iconProps={{ iconName: "Bold" }}
-            onClick={this.action.bold}
-          />
-          <IconButton
-            iconProps={{ iconName: "Italic" }}
-            onClick={this.action.italic}
-          />
+          <Stack.Item grow disableShrink>
+            <TooltipHost content="Markdown Editor">
+              <IconButton
+                iconProps={{ iconName: 'MarkDownLanguage' }}
+                styles={{
+                  icon: {
+                    color: CommunicationColors.primary + ' !important',
+                    fontWeight: 'bolder'
+                  }
+                }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Header">
+              <IconButton
+                iconProps={{ iconName: 'EditStyle' }}
+                onRenderMenuIcon={() => false}
+                menuProps={{
+                  shouldFocusOnMount: true,
+                  items: [
+                    {
+                      iconProps: { iconName: 'Header1' },
+                      text: 'Header 1',
+                      key: '1'
+                    },
+                    {
+                      iconProps: { iconName: 'Header2' },
+                      text: 'Header 2',
+                      key: '2'
+                    },
+                    {
+                      iconProps: { iconName: 'Header3' },
+                      text: 'Header 3',
+                      key: '3'
+                    },
+                    {
+                      iconProps: { iconName: 'Header4' },
+                      text: 'Header 4',
+                      key: '4'
+                    }
+                  ]
+                }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Bold">
+              <IconButton
+                iconProps={{ iconName: 'Bold' }}
+                onClick={this.action.bold}
+              />
+            </TooltipHost>
+            <TooltipHost content="Italic">
+              <IconButton
+                iconProps={{ iconName: 'Italic' }}
+                onClick={this.action.italic}
+              />
+            </TooltipHost>
+            <TooltipHost content="Strikethrough">
+              <IconButton
+                iconProps={{ iconName: 'Strikethrough' }}
+                onClick={this.action.strikethrough}
+              />
+            </TooltipHost>
+            <TooltipHost content="Blockquotes">
+              <IconButton
+                iconProps={{ iconName: 'RightDoubleQuote' }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Bulleted List">
+              <IconButton
+                iconProps={{ iconName: 'BulletedList' }}
+                onClick={this.action.strikethrough}
+              />
+            </TooltipHost>
+            <TooltipHost content="Numbered List">
+              <IconButton
+                iconProps={{ iconName: 'NumberedList' }}
+                onClick={this.action.strikethrough}
+              />
+            </TooltipHost>
+            <TooltipHost content="Task List">
+              <IconButton
+                iconProps={{ iconName: 'CheckList' }}
+                onClick={this.action.strikethrough}
+              />
+            </TooltipHost>
+            <TooltipHost content="Table">
+              <IconButton
+                iconProps={{ iconName: 'Table' }}
+                onRenderMenuIcon={() => false}
+                menuProps={{
+                  onRenderMenuList: (menuListProps, defaultRender) => (
+                    <Stack>
+                      <Text className={classNames.headerTable} block>{`8x8 table`}</Text>
+                      <table className={classNames.tableWrapper}>
+                        <tbody>
+                          {[...Array(10)].map((row, rowIndex) => (
+                            <tr key={`row-${rowIndex}`}>
+                              {[...Array(10)].map((column, columnIndex) => (
+                                <td
+                                  key={`column-${columnIndex}`}
+                                >
+                                  <div className={classNames.tableItem}></div>
+                                </td>
+                              ))}
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                      {defaultRender(menuListProps)}
+                    </Stack>
+                  ),
+                  shouldFocusOnMount: true,
+                  items: [
+                    {
+                      iconProps: { iconName: 'Table' },
+                      text: 'Manual',
+                      key: '1'
+                    }
+                  ]
+                }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Code">
+              <IconButton
+                iconProps={{ iconName: 'CodeEdit' }}
+                onRenderMenuIcon={() => false}
+                menuProps={{
+                  shouldFocusOnMount: true,
+                  items: [
+                    {
+                      iconProps: { iconName: 'CalculatorSubtract' },
+                      text: 'Inline',
+                      key: '1'
+                    },
+                    {
+                      iconProps: { iconName: 'CollapseMenu' },
+                      text: 'Block',
+                      key: '2'
+                    }
+                  ]
+                }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Link">
+              <IconButton
+                iconProps={{ iconName: 'Link' }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Image">
+              <IconButton
+                iconProps={{ iconName: 'Photo2' }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Video">
+              <IconButton
+                iconProps={{ iconName: 'Video' }}
+              />
+            </TooltipHost>
+          </Stack.Item>
+          <Stack.Item>
+            <TooltipHost content="Documentation">
+              <IconButton
+                iconProps={{ iconName: 'TextDocument' }}
+              />
+            </TooltipHost>
+            <TooltipHost content="Save">
+              <IconButton iconProps={{ iconName: 'Save' }} />
+            </TooltipHost>
+          </Stack.Item>
         </Stack>
         <div
           ref={this.containerDidMount}
--- src/components/MonacoEditor/MonacoEditor.style.js
+++ src/components/MonacoEditor/MonacoEditor.style.js
@@ -12,6 +12,20 @@ export const getStyle = () => {
           color: '#444444'
         }
       }
+    },
+    tableWrapper: {
+      margin: '0.3rem 0.3rem 0rem 0.3rem',
+      paddingBottom: '0.5rem',
+      borderBottom: '1px solid rgb(243, 242, 241)'
+    },
+    headerTable: {
+      margin: '0.3rem auto 0rem auto'
+    },
+    tableItem: {
+      width: 10, 
+      height: 10, 
+      padding: 1, 
+      border: '1px solid #444444'
     }
   })
 }
