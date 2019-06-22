13e335c6b202df6c530970c505700f52ee944576
quanghuy1242 <katygaga124@gmail.com>
2019-06-22 09:51:20 +0700

    update: fabric 6 -> 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Customizer } from 'office-ui-fabric-react';
-import { FluentCustomizations } from '@uifabric/fluent-theme';
 import { RouteContainer } from './components/RouteContainer/RouteContainer';
 import { MainApp } from './components/MainApp/MainApp';
 
 class App extends React.Component {
   render() {
     return (
-      <Customizer {...FluentCustomizations}>
-        <MainApp>
-          <RouteContainer />
-        </MainApp>
-      </Customizer>
+      <MainApp>
+        <RouteContainer />
+      </MainApp>
     );
   }
 }
