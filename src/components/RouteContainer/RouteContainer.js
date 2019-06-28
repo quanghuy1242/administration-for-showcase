5142ddb43a01d3716f1d52c9255c25b195ca8d27
quanghuy1242 <katygaga124@gmail.com>
2019-06-28 16:00:14 +0700

    changed: replaced Public Route with Private Route

--- src/components/RouteContainer/RouteContainer.js
+++ src/components/RouteContainer/RouteContainer.js
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import { Dashboard } from '../../pages/Dashboard/Dashboard';
 import { Login } from '../../pages/Login/Login';
 import { LandscapeHome } from '../../pages/LandscapeHome/LandscapeHome';
+import { PrivateRoute } from '../PrivateRoute/PrivateRoute';
 
 export class RouteContainer extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={LandscapeHome} />
-        <Route exact path="/projects" component={Dashboard} />
+        <PrivateRoute exact path="/" component={LandscapeHome} />
+        <PrivateRoute exact path="/projects" component={Dashboard} />
         <Route exact path="/login" component={Login} />
       </Switch>
     );
