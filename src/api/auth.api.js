1cd50b6a78d0f5854ac7018f3975e87ca4c8fe7c
quanghuy1242 <katygaga124@gmail.com>
2019-06-28 16:02:50 +0700

    changed: indent added

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -3,13 +3,13 @@ import api from './api';
 export class AuthApi {
   static isAuthenticated() {
     api.post('/auth/isAuthenticated')
-    .then(response => {
-      console.log(response);
-      return true;
-    })
-    .catch(err => {
-      console.log(err.response);
-      return false;
-    }); 
+      .then(response => {
+        console.log(response);
+        return true;
+      })
+      .catch(err => {
+        console.log(err.response);
+        return false;
+      }); 
   }
 }
