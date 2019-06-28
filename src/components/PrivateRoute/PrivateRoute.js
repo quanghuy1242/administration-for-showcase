727b92369230cfc8ecee1515b14306e9294262a4
quanghuy1242 <katygaga124@gmail.com>
2019-06-28 18:22:54 +0700

    change: complete private route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { AuthApi } from '../../api/auth.api';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => 
-      AuthApi.isAuthenticated() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to='/login'
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchData() {
+      const isAuthenticated = await AuthApi.isAuthenticated();
+      if (isAuthenticated) { setIsAuthenticated(true) }
+      else setIsAuthenticated(false);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    isLoading
+      ? (
+        <div style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          backgroundColor: 'white',
+          zIndex: 999999999999999,
+          display: "flex",
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          {/* TODO Splash View */}
+          Loading...
+        </div>
+      )
+      : (
+        <Route
+          {...rest}
+          render={props => 
+            isAuthenticated ? (
+              <Component {...props} />
+            ) : (
+              <Redirect
+                to='/login'
+              />
+            )}
         />
-      )}
-  />
-)
+      )
+  )
+}
