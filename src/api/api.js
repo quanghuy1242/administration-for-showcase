89cb7a45c4cbd9ef6094b490d9b1e0d4d84b903e
quanghuy1242 <katygaga124@gmail.com>
2019-06-28 18:17:22 +0700

    changed: add content-type, credential send to server by default

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "https://project-showcase-api.herokuapp.com",
-  responseType: "json"
+  responseType: "json",
 }); 
+
+instance.defaults.headers.post['Content-Type'] = 'application/json';
+instance.defaults.withCredentials = true;
+
+export default instance;
