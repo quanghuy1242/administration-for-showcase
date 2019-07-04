2d959d8225eff86a661114e0300c4e0bf3654c7e
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-04 16:34:41 +0700

    changed: temple axios interceptor for response

--- src/api/api.js
+++ src/api/api.js
@@ -11,19 +11,34 @@ const isFetching = false; // ngăn các request khác cũng chạy phần refres
 instance.defaults.headers.post['Content-Type'] = 'application/json';
 instance.defaults.withCredentials = true;
 
-// instance.interceptors.response.use(undefined, async err => {
-//   if (err.response.status === 403) return await AuthApi.logout();
-//   if (err.response.status !== 401) return Promise.reject(err);
-  
-//   if (!isFetching) {
-//     isFetching = true; // lỡ mà có nhiều request thì chặn cái sau refresh
-//     try {
-//       await AuthApi.refreshToken();
-//       return Promise.resolve(axios(err.config));
-//     } catch (error) {
-//       return await AuthApi.logout();
-//     }
-//   }
-// });
+function logout() {
+  try {
+    (async () => {
+      await AuthApi.logout();
+      window.location = '/';
+    })();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+instance.interceptors.response.use(undefined, err => {
+  return new Promise(async (resolve, reject) => {
+    if (err.config.url === '/auth/logout') return reject(err);
+    if (err.response.status === 403) return resolve(logout);
+    if (err.response.status !== 401) return reject(err);
+    
+    try {
+      if (!isFetching) {
+        isFetching = true;
+        await AuthApi.refreshToken();
+        return resolve(axios(err.config));
+      }
+    } catch (error) {
+      console.error(error);
+      return resolve(logout);
+    }
+  });
+});
 
 export default instance;
