b90d6248d5497a6b424790a11ccac142623c5f10
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-05 15:41:48 +0700

    changed: tech detail with basic detail

--- src/components/TechDetail/TechDetail.js
+++ src/components/TechDetail/TechDetail.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Panel, PanelType, TextField, PrimaryButton, DefaultButton } from 'office-ui-fabric-react';
+import { Panel, PanelType, TextField, PrimaryButton, DefaultButton, Image } from 'office-ui-fabric-react';
+import { getStyle } from './TechDetail.style';
 
 export class TechDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tech: {
-        id: '',
+        nameId: '',
         name: '',
-        date: null
+        description: '',
+        imageUrl: ''
       },
     };
   }
@@ -23,6 +25,20 @@ export class TechDetail extends React.Component {
   handleNameChanged = (event) => {
     this.setState(state => {
       state.tech.name = event.target.value;
+      return state;
+    })
+  }
+
+  handleDesChanged = event => {
+    this.setState(state => {
+      state.tech.description = event.target.value;
+      return state;
+    })
+  }
+
+  handleImageUrlChanged = event => {
+    this.setState(state => {
+      state.tech.imageUrl = event.target.value;
       return state;
     })
   }
@@ -50,6 +66,7 @@ export class TechDetail extends React.Component {
   };
 
   render() {
+    const classNames = getStyle();
     return (
       <Panel
         isOpen={this.props.isOpen}
@@ -58,8 +75,14 @@ export class TechDetail extends React.Component {
         closeButtonAriaLabel="Close"
         onRenderFooterContent={this.handleRenderFooterContent}
       >
+        <Image
+          src={this.state.tech.imageUrl}
+          alt="Image Preview"
+          width="100%"
+          className={classNames.imagePreview}
+        />
         <TextField
-          label="Id"
+          label="Identify Name"
           value={this.state.tech.id}
           onChange={this.handleIdChanged}
         />
@@ -67,6 +90,17 @@ export class TechDetail extends React.Component {
           label="Name of technologoy"
           value={this.state.tech.name}
           onChange={this.handleNameChanged}
+        />
+        <TextField
+          label="Description"
+          multiline={true}
+          value={this.state.tech.description}
+          onChange={this.handleDesChanged}
+        />
+        <TextField
+          label="Image URL"
+          value={this.state.tech.imageUrl}
+          onChange={this.handleImageUrlChanged}
         />
       </Panel>
     );
