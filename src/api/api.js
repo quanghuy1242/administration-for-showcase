0f2a08383d947879e1d92e5447898d7799ba0284
quanghuy1242 <quanghuy1242@gmail.com>
2019-08-01 22:55:58 +0700

    comment unuse code

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { AuthApi } from './auth.api';
+// import { AuthApi } from './auth.api';
 
 const instance = axios.create({
   baseURL: "https://project-showcase-api.herokuapp.com",
   responseType: "json",
   timeout: 30000
 }); 
-const isFetching = false; // ngăn các request khác cũng chạy phần refresh token
+// const isFetching = false; // ngăn các request khác cũng chạy phần refresh token
 
 instance.defaults.headers.post['Content-Type'] = 'application/json';
 instance.defaults.withCredentials = true;
