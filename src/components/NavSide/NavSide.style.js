cd883a4581aae7304def071d60fb6c0bee18345d
quanghuy1242 <quanghuy1242@gmail.com>
2019-09-06 16:35:50 +0700

    fixed style for new specific of fluent design

--- src/components/NavSide/NavSide.style.js
+++ src/components/NavSide/NavSide.style.js
@@ -30,7 +30,7 @@ export const getStyle = ({ isCollapsed, isOverlay }) => {
     navInnerWrapper: {
       selectors: {
         '& ul': {
-          marginTop: '0.6rem'
+          marginTop: '0.2rem'
         }
       }
     }
