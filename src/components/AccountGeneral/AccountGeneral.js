59688bdeff9310e02b784d1207666c2f6a38fa46
quanghuy1242 <quanghuy1242@gmail.com>
2019-09-08 18:05:05 +0700

    ajuit layout

--- src/components/AccountGeneral/AccountGeneral.js
+++ src/components/AccountGeneral/AccountGeneral.js
@@ -8,37 +8,46 @@ class AccountGeneral extends React.Component {
     return (
       <Stack className={classNames.wrapper}>
         <Text variant='xLarge'>General</Text>
-        <Text>Các thông tin cơ bản</Text>
         <Stack horizontal tokens={{ childrenGap: 32 }}>
           <Stack.Item>
-            <Stack style={{ width: 500 }} className={classNames.mainInput}>
-              <TextField
-                label='Name'
-              />
-              <TextField
-                label='Display Name'
-              />
-              <TextField
-                label='Image'
-              />
-              <TextField
-                label='Cover Image'
-              />
-              <TextField
-                label='Facebook'
-              />
-              <TextField
-                label='Twitter'
-              />
-              <TextField
-                label='Instagram'
-              />
-              <TextField
-                label='Wordpress'
-              />
-              <TextField
-                label='Github'
-              />
+            <Stack style={{ width: 500 }} className={classNames.mainInput} tokens={{ childrenGap: 16 }}>
+              <Stack>
+                <Text variant='xLarge'>Các thông tin cơ bản</Text>
+                <TextField
+                  label='Username'
+                  description='Tên administrator, có thể hiểu là tên đăng nhập'
+                />
+                <TextField
+                  label='Display Name'
+                  description='Tên hiển thị, có thể sử dụng một cái tên đẹp'
+                />
+                <TextField
+                  label='Image'
+                  description='Đường dẫn đến hình ảnh của ảnh đại diện'
+                />
+                <TextField
+                  label='Cover Image'
+                  description='Đường dẫn đến hình ảnh của ảnh bìa'
+                />
+              </Stack>
+              <Stack>
+                <Text variant='xLarge'>Liên kết mạng xã hội</Text>
+                <TextField
+                  label='Facebook'
+                />
+                <TextField
+                  label='Twitter'
+                />
+                <TextField
+                  label='Instagram'
+                />
+                <TextField
+                  label='Wordpress'
+                />
+                <TextField
+                  label='Github'
+                />
+              </Stack>
               <Stack horizontalAlign='end' style={{ marginTop: 8 }}>
                 <PrimaryButton
                   text='Save'
